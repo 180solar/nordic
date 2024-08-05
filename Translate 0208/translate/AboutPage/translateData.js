ef51609732db946271ea1763dd4969e05afba3e9
Solar 180 <123119042+180solar@users.noreply.github.com>
2024-08-05 15:03:40 +0200

    Add files via upload

---
+++ Translate 0208/translate/AboutPage/translateData.js
@@ -0,0 +1,77 @@
+export const translateData = [
+    {
+        code: 'gb',
+        main: {
+            title: ['Consulting on', 'International Tax'],
+            btnText: 'Discover more'
+        },
+        realEstate: {
+            title: 'Simplify the complexity of tax regulations',
+            subTitle: 'ABOUT US',
+            text1: 'In today’s business environment, international expansion is more than a possibility: it’s a necessity for growth and survival. However, this process comes with a series of complex tax challenges that can complicate the efficient operation of companies in multiple jurisdictions. This is where international tax consulting plays a crucial role, providing strategic guidance to maximize benefits and minimize tax liabilities in a global context.',
+            text2: 'International tax consulting encompasses a wide range of services designed to help companies navigate the complex international tax landscape. From cross-border tax planning to resolving international tax disputes, experts in this field work closely with their clients to develop customized strategies that align with their business objectives and comply with local and international tax regulations.',
+        },
+        strategies: {
+            title: 'Real Estate Investment Consulting',
+            item1: 'Maximizing Your Return in the Global Real Estate Market',
+            item2: 'Experts in Personalized Investment Strategies for Real Estate Success',
+            stroke: ['Connecting your global opportunities: Tax consulting,', 'languages, investments, and finance.']
+        },
+        contatcs: {
+            title: 'Hello! Let’s connect and create something great together. How about starting by saying hello?',
+            phoneInput: 'Your name',
+            noteInput: 'Your message',
+            btnText: 'Send message'
+        }
+    },
+    {
+        code: 'sv',
+        main: {
+            title: ['Rådgivning om', 'Internationell Skatt'],
+            btnText: 'Upptäck mer'
+        },
+        realEstate: {
+            title: 'Förenkla komplexiteten i skatteregler',
+            subTitle: 'OM OSS',
+            text1: 'I dagens affärsmiljö är internationell expansion mer än en möjlighet: det är en nödvändighet för tillväxt och överlevnad. Men denna process kommer med en serie komplexa skatteutmaningar som kan komplicera företagens effektiva drift i flera jurisdiktioner. Det är här internationell skatterådgivning spelar en avgörande roll, genom att ge strategisk vägledning för att maximera fördelar och minimera skatteförpliktelser i ett globalt sammanhang.',
+            text2: 'Internationell skatterådgivning omfattar ett brett spektrum av tjänster utformade för att hjälpa företag att navigera i det komplexa internationella skattelandskapet. Från gränsöverskridande skatteplanering till att lösa internationella skattetvister, experter inom detta område arbetar nära sina kunder för att utveckla skräddarsydda strategier som är i linje med deras affärsmål och uppfyller lokala och internationella skatteregler.',
+        },
+        strategies: {
+            title: 'Rådgivning om Fastighetsinvesteringar',
+            item1: 'Maximera din avkastning på den globala fastighetsmarknaden',
+            item2: 'Experter på Personliga Investeringsstrategier för Fastighetsframgång',
+            stroke: ['Koppla dina globala möjligheter: Skatterådgivning,', 'språk, investeringar och finans.']
+        },
+        contatcs: {
+            title: 'Hej! Låt oss ansluta och skapa något fantastiskt tillsammans. Vad sägs om att börja med att säga hej?',
+            phoneInput: 'Ditt namn',
+            noteInput: 'Ditt meddelande',
+            btnText: 'Skicka meddelande'
+        }
+    },
+    {
+        code: 'es',
+        main: {
+            title: ['Asesoramiento en', 'Fiscal Internacional'],
+            btnText: 'Descubre mas'
+        },
+        realEstate: {
+            title: 'Simplifica la complejidad de las normativas fiscales ',
+            subTitle: 'SOBRE NOSOTROS',
+            text1: 'En el entorno empresarial actual, la expansión internacional es más que una posibilidad: es una necesidad para el crecimiento y la supervivencia. Sin embargo, este proceso conlleva una serie de desafíos fiscales complejos que pueden complicar la operación eficiente de las empresas en múltiples jurisdicciones. Es aquí donde el asesoramiento fiscal internacional desempeña un papel crucial, brindando orientación estratégica para maximizar los beneficios y minimizar las obligaciones fiscales en un contexto global.',
+            text2: 'El asesoramiento fiscal internacional abarca una amplia gama de servicios diseñados para ayudar a las empresas a navegar por el complejo panorama fiscal internacional. Desde la planificación fiscal transfronteriza hasta la resolución de disputas fiscales internacionales, los expertos en este campo trabajan en estrecha colaboración con sus clientes para desarrollar estrategias personalizadas que se alineen con sus objetivos comerciales y cumplan con las regulaciones fiscales locales e internacionales.',
+        },
+        strategies: {
+            title: 'Asesoramiento en Inversiones Inmobiliarias',
+            item1: 'Maximizando su Retorno en el Mercado Inmobiliario Global',
+            item2: 'Expertos en Estrategias de Inversión Personalizadas para el Éxito Inmobiliario',
+            stroke: ['Conectando tus oportunidades globales: Asesoramiento', 'fiscal, idiomas, inversiones y finanzas. ']
+        },
+        contatcs: {
+            title: '¡Hola! Conectemos y creemos algo grandioso juntos. ¿Qué tal si empiezamos diciendonos hola?',
+            phoneInput: 'Su nombre',
+            noteInput: 'Su mensaje',
+            btnText: 'Enviar mensaje'
+        }
+    },
+]
